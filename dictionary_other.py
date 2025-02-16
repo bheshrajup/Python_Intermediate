6b907dc93a93aed9b0d643852449aab6ba547628
Bheshraj Upadhyaya <achinarubheshraj01@gmail.com>
2025-02-16 17:23:14 +0545

    Copy methods in dictionary in python

---
+++ dictionary_other.py
@@ -0,0 +1,55 @@
+#  Dictionary
+Aperson = {
+    "first_name": "John",
+    "last_name": "Doe",
+    "age": 23,
+    "jobs": ["Software Engineer", "Teacher"],
+    # "job": "Teacher"  # Duplicate key will be overwritten
+}
+# Dictionary type because key-value pair
+# print(f"Type of Aperson is: {type(Aperson)}")
+# print(f"Person is:{Aperson}")
+
+# print(f"---------Copy in dictionary---------")
+# person = Aperson
+# print(f"Other person is also:{person}")
+
+# print(f"---------After changing first name in Aperson---------")
+# Aperson["first_name"]="Bheshraj"
+# print(f"Person is:{Aperson}")
+# print(f"Other person is also:{person}")
+# #the first name is changed in both....copied
+
+# print(f"---------After changing first name in person---------")
+# person["first_name"]="Achinaru"
+# print(f"Person is:{person}")
+# print(f"Other person is also:{Aperson}")
+# #the first name is changed in both...copied
+
+
+# person = Aperson.copy()
+# print(f"Other person is also:{person}")
+
+
+# print(f"---------After changing first name in Aperson---------")
+# Aperson["first_name"]="Bheshraj"
+# print(f"Person is:{Aperson}")
+# print(f"Other person is also:{person}")
+# #the first name is not changed in new variable....
+
+
+# print(f"---------After changing first name in person---------")
+# person["first_name"]="Achinaru"
+# print(f"Person is:{person}")
+# print(f"Other person is also:{Aperson}")
+# #the first name is not changed in both...
+
+
+person = dict(Aperson)
+print(f"Other person is also:{person}")
+
+
+print(f"---------After changing first name in Aperson---------")
+Aperson["first_name"]="Bheshraj"
+print(f"Person is:{Aperson}")
+print(f"Other person is also:{person}")
