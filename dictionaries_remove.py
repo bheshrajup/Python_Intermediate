e4993d0ce911a27ee0b469c20f3c530ad3091e89
Bheshraj Upadhyaya <achinarubheshraj01@gmail.com>
2025-02-16 16:52:00 +0545

    Remove operation on dictionaries in python

---
+++ dictionaries_remove.py
@@ -0,0 +1,11 @@
+# Dictionary
+Aperson = {
+    "first_name": "John",
+    "last_name": "Doe",
+    "age": 23,
+    "jobs":["Software Engineer","Teacher"],
+    # "job": "Teacher"  # Duplicate key will be overwritten
+}
+# Dictionary type because key-value pair
+print(f"Type of Aperson is: {type(Aperson)}")
+print(f"Person is:{Aperson}")
