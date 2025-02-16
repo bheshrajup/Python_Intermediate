036e9adb291a1f60978b837b4b022edbd1ded32e
Bheshraj Upadhyaya <achinarubheshraj01@gmail.com>
2025-02-16 18:07:33 +0545

    Loops in dicitonary in python

--- dictionary_other.py
+++ dictionary_other.py
@@ -7,8 +7,8 @@ Aperson = {
     # "job": "Teacher"  # Duplicate key will be overwritten
 }
 # Dictionary type because key-value pair
-# print(f"Type of Aperson is: {type(Aperson)}")
-# print(f"Person is:{Aperson}")
+print(f"Type of Aperson is: {type(Aperson)}")
+print(f"Person is:{Aperson}")
 
 # print(f"---------Copy in dictionary---------")
 # person = Aperson
@@ -45,11 +45,43 @@ Aperson = {
 # #the first name is not changed in both...
 
 
-person = dict(Aperson)
-print(f"Other person is also:{person}")
+# person = dict(Aperson)
+# print(f"Other person is also:{person}")
 
 
-print(f"---------After changing first name in Aperson---------")
-Aperson["first_name"]="Bheshraj"
-print(f"Person is:{Aperson}")
-print(f"Other person is also:{person}")
+# print(f"---------After changing first name in Aperson---------")
+# Aperson["first_name"]="Bheshraj"
+# print(f"Person is:{Aperson}")
+# print(f"Other person is also:{person}")
+
+
+print(f"---------------Loops in dictionary------------------")
+print("--------All the key in Aperson dictionary--------------")
+for k in Aperson:
+    print(k)
+
+print("--------All the key using keys method--------------")
+
+for k in Aperson.keys():
+    print(k)
+
+
+print(f"----------All the value in Aperson dictionary---------")
+
+for k in Aperson:
+    print(Aperson[k])
+
+print("--------All the values using values method--------------")
+
+for k in Aperson.values():
+    print(k)
+
+
+print("--------All the key-values using items method--------------")
+
+for k in Aperson.items():
+    print(k)
+print("---All the key-values using items method in formatted method---")
+
+for k,v in Aperson.items():
+    print(k,v)
