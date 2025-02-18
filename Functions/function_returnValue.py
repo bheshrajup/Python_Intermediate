520a55f49f687018a8fc13c249b427526d80bdd2
Bheshraj Upadhyaya <achinarubheshraj01@gmail.com>
2025-02-18 13:36:08 +0545

    Return statement in functions in python

---
+++ Functions/function_returnValue.py
@@ -0,0 +1,22 @@
+# ------------Return value-------------
+def sum(x, y):
+    result = x+y
+    return result
+
+
+result_sum = sum(2, 3)
+print(f"Result of sum is=> {result_sum}")
+
+result_sum = sum(2.5, 10.5)
+print(f"Result of sum is=> {result_sum}")
+
+
+result_sum = sum("Hello", "friend")
+print(f"Result of sum is=> {result_sum}")
+
+
+print(f"50+40:{sum(50,40)}")
+
+
+def subtract(x,y):
+    pass 
